72641c4af0cc70bb13000a047defe125fac5ff0d
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-10-26 23:19:24 -0500

    make it harder to forget to throw an error

--- lib/DBIx/ParseException/SQLite.pm
+++ lib/DBIx/ParseException/SQLite.pm
@@ -7,22 +7,18 @@ with 'DBIx::ParseException';
 sub error_handler {
    my $string = shift;
    my @args = ( original => $string );
+   my $class = 'DBIx::Exception';
 
    if ($string =~ /^DBD::SQLite::st \s+ execute \s+ failed: \s+ column \s+ (.*)
                     \s+ is \s+ not \s+ unique/ixm) {
-      DBIx::Exception::NotUnique->throw(
-         column => $1,
-         @args,
-      );
+      $class .= '::NotUnique';
+      push @args, ( column => $1 );
    } elsif ($string =~ /^DBD::SQLite::db \s+ prepare \s+ failed: \s+ near \s+
                         "(.*)": \s+ syntax \s+ error/ixm) {
-      DBIx::Exception::Syntax->throw(
-         near => $1,
-         @args,
-      );
-   } else {
-      DBIx::Exception->throw(@args);
+      $class .= '::Syntax';
+      push @args, ( near => $1 );
    }
+   $class->throw(@args);
 }
 
 1;
