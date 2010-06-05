2afb1217fb8afb3794f35c45ddb2530c14231c90
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-06-05 13:49:55 -0500

    add capabilities

--- lib/DBIx/ParseException.pm
+++ lib/DBIx/ParseException.pm
@@ -5,19 +5,23 @@ use warnings;
 use DBI;
 use Carp 'croak';
 
+sub _get_driver {
+  return do {
+      if (my $dbh = $_[1]->{dbh}) {
+      $dbh->{Driver}{Name};
+    } else {
+      ( DBI->parse_dsn($_[1]->{dsn}) )[1];
+    }
+  }
+}
+
 DRIVERS: {
    my %DRIVERS;
 
    sub handler {
       my $self = shift;
       my $params = shift or croak 'params are required for DBIx::ParseException!';
-      my $driver = do {
-         if (my $dbh = $params->{dbh}) {
-            $dbh->{Driver}{Name};
-         } else {
-            ( DBI->parse_dsn($params->{dsn}) )[1];
-         }
-      };
+      my $driver = $self->_get_driver($params);
       return $DRIVERS{$driver} ||= do {
          my $parser = __PACKAGE__ . "::$driver";
          eval "require $parser";
@@ -27,4 +31,22 @@ DRIVERS: {
    }
 }
 
+CAPABILITIES: {
+   my %CAPABILITIES;
+
+   sub capabilities {
+      my $self = shift;
+      my $params = shift or croak 'params are required for DBIx::ParseException!';
+      my $driver = $self->_get_driver($params);
+      return $CAPABILITIES{$driver} ||= do {
+         my $parser = __PACKAGE__ . "::$driver";
+         eval "require $parser";
+         die $@ if $@;
+         $parser->can('capabilities')->($parser);
+      };
+   }
+}
+
 1;
+
+# vim: ts=2 tw=2 expandtab
