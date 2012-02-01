688aed06ee051ed3139e93284ca03532f02d1a2e
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-01-31 20:56:03 -0600

    return hashref of info, not string

--- lib/DBIx/ParseException/Role/ExtractFromDBH.pm
+++ lib/DBIx/ParseException/Role/ExtractFromDBH.pm
@@ -6,67 +6,40 @@ use DBI;
 sub extract_from_dbh {
    my ($self, $dbh) = @_;
 
-   my $out;
+   my %out;
    if ($DBI::lasth) {
-      $out .= "  lasth type: $DBI::lasth->{Type}\n"
-         if ($DBI::lasth->{Type});
-      $out .= "  lasth Statement ($DBI::lasth):\n    " .
-                                                "$DBI::lasth->{Statement}\n"
-                                                   if ($DBI::lasth->{Statement});
-   }
-   # get db handle if we have an st
-   my $type = $dbh->{Type};
-   my $sql;
-   if ($type eq 'st') {    # given statement handle
-      $dbh = $dbh->{Database};
-      $sql = $dbh->{Statement};
-   } else {
-   # given db handle
-   # We've got other stmts under this db but we'll deal with those later
-      $sql = 'Possible SQL: ';
-      $sql .= "/$dbh->{Statement}/" if $dbh->{Statement};
+      $out{lasth}           = $DBI::lasth;
+      $out{lasth_type}      = $DBI::lasth->{Type};
+      $out{lasth_statement} = $DBI::lasth->{Statement};
    }
 
-   my $dbname = exists($dbh->{Name}) ? $dbh->{Name} : "";
-   my $username = exists($dbh->{Username}) ? $dbh->{Username} : "";
-   $out .= "  DB: $dbname, Username: $username\n";
-   $out .= "  handle type: $type\n  SQL: " . DBI::neat($sql) . "\n";
-   $out .= '  db Kids=' . $dbh->{Kids} .  ', ActiveKids=' . $dbh->{ActiveKids} . "\n";
-   $out .= "  DB errstr: " . $dbh->errstr . "\n";
-
+   my $type = $dbh->{Type} || '';
    if ($type eq 'st') {
-      my $str = "";
-      if ($dbh->{ParamValues}) {
-         foreach (sort keys %{$dbh->{ParamValues}}) {
-            $str .= "$_=" . DBI::neat($dbh->{ParamValues}->{$_}) . ",";
-         }
-      }
-      $out .= "  ParamValues: $str\n";
-      $out .= "  " . Data::Dumper->Dump([$dbh->{ParamArrays}], ['ParamArrays'])
-         if ($dbh->{ParamArrays});
-   }
-   my @substmts;
-   # get list of statements under the db
-   push @substmts, $_ for (grep { defined } @{$dbh->{ChildHandles}});
-   $out .= "  " . scalar(@substmts) . " sub statements:\n";
-   if (scalar(@substmts)) {
-      foreach my $stmt (@substmts) {
-         $out .= "  stmt($stmt):\n";
-         $out .= '    SQL(' . $stmt->{Statement} . ")\n  " if $stmt->{Statement};
-               if (exists($stmt->{ParamValues}) && $stmt->{ParamValues}) {
-               $out .= '   Params(';
-                  foreach (sort keys %{$stmt->{ParamValues}}) {
-                     $out .= "$_=" . DBI::neat($stmt->{ParamValues}->{$_}) . ",";
-                  }
-                  $out .= ")\n";
-               }
-      }
+      $dbh               = $dbh->{Database};
+      $out{param_values} = $dbh->{ParamValues};
+      $out{param_arrays} = $dbh->{ParamArrays};
    }
 
-   if (exists($dbh->{Callbacks})) {
-      $out .= "  Callbacks exist for " .
-      join(",", keys(%{$dbh->{Callbacks}})) . "\n";
-   }
+   $out{name}         = $dbh->{Name};
+   $out{username}     = $dbh->{Username};
+   $out{type}         = $type;
+   $out{statement}    = $dbh->{Statement};
+   $out{kids}         = $dbh->{Kids};
+   $out{active_kids}  = $dbh->{ActiveKids};
+   $out{error_string} = $dbh->errstr;
+   $out{callbacks}    = $dbh->{Callbacks} || {};
+
+   # this should probably be:
+   # @substmts = map $self->extract_from_dbh($stmt),
+   #    grep { defined } @{$dbh->{ChildHandles}};
+   #
+   # but we'll start with what we already have
+   $out{substatements} = [map +{
+      statement    => $_->{Statement},
+      param_values => $_->{ParamValues},
+   }, grep { defined } @{$dbh->{ChildHandles}}];
+
+   \%out
 }
 
 1;
