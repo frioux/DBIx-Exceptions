471f870ad5624b2f06e8da22f9ce08f1cfe08848
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-02-07 21:34:03 -0600

    POD

--- lib/DBIx/ParseException.pm
+++ lib/DBIx/ParseException.pm
@@ -3,6 +3,8 @@ package DBIx::ParseException;
 use strict;
 use warnings;
 
+# ABSTRACT: Parse exceptions into objects
+
 use Module::Load;
 use Role::Tiny;
 use Carp 'croak';
@@ -33,3 +35,58 @@ sub new {
 
 1;
 
+
+=pod
+
+=head1 SYNOPSIS
+
+ my $parser = DBIx::ParseException->new({
+    database => 'SQLite',
+    api      => 0,
+ });
+
+ $dbh->{HandleError} = sub { die($parser->parse(@_)) };
+
+=head1 DESCRIPTION
+
+This module is for converting all the myriad bits of information L<DBI>
+L<provides|DBI/HandleError> when an error occurs into a nice, structured
+object.
+
+Currently the API is very simple, one constructs a new parser by calling
+L</new>, and then calling L<parse|DBIx::ParseException::Role::API0/parse> with
+the information that gets passed to L<HandleError|DBI/HandleError>.
+
+=head1 IMPORTANT NOTE REGARDING DATA EXTRACTION
+
+Although the API you use may provide a slot for a piece of data, the data may
+be blank nonetheless.  The data may be unavailable for your database, or this
+module may not take advantage of special DBD features yet.
+
+=head1 METHODS
+
+=head2 new
+
+ DBIx::ParseException->new({
+    database => 'SQLite',
+    api      => 0,
+ });
+
+Arguments:
+
+=over
+
+=item C<api>
+
+B<required>, this defines the api version to use.  See the role in
+L<DBIx::ParseException::Role> with the version of the api you request for
+documentation.  The API ultimately decides what kinds of Exception objects
+you get back.
+
+=item C<database>
+
+B<required>, this defines which database type you are "parsing."  Note that
+there may be more required parameters for a given database, as it may need to
+know, for example, the version of the database or the connection method.
+
+=back
--- lib/DBIx/ParseException/Role/API0.pm
+++ lib/DBIx/ParseException/Role/API0.pm
@@ -1,7 +1,127 @@
 package DBIx::ParseException::Role::API0;
 
+# ABSTRACT: Version 0 of the exception types
+
 use Moo::Role;
 
 with 'DBIx::ParseException::Role::API';
 
 1;
+
+=pod
+
+=head1 DBIx::Exception
+
+=head2 original
+
+The string that got passed to the parser
+
+=head2 lasth
+
+L<DBI/lasth>
+
+=head2 type
+
+L<DBI/Type>
+
+=head2 param_values
+
+L<DBI/ParamValues>
+
+=head2 param_arrays
+
+L<DBI/ParamArrays>
+
+=head2 name
+
+L<DBI/Name>
+
+=head2 username
+
+L<DBI/Username>
+
+=head2 statement
+
+L<DBI/Statement>
+
+=head2 kids
+
+L<DBI/Kids>
+
+=head2 active_kids
+
+L<DBI/ActiveKids>
+
+=head2 error_string
+
+L<DBI/errstr>
+
+=head2 callbacks
+
+L<DBI/Callbacks>
+
+=head2 substatements
+
+=head1 DBIx::Exception::ConstraintViolation
+
+Inherits from L</DBIx::Exception>.
+
+=head2 column
+
+The column that caused the constraint violation
+
+=head2 constraint
+
+The constraint that was violated
+
+=head1 DBIx::Exception::NotUnique
+
+Inherits from L</DBIx::Exception::ConstraintViolation>.
+
+=head1 DBIx::Exception::NotNull
+
+Inherits from L</DBIx::Exception::ConstraintViolation>.
+
+=head1 DBIx::Exception::ForeignKey
+
+Inherits from L</DBIx::Exception::ConstraintViolation>.
+
+=head1 DBIx::Exception::Timeout
+
+Inherits from L</DBIx::Exception>.
+
+=head1 DBIx::Exception::QueryCanceled
+
+Inherits from L</DBIx::Exception>.
+
+=head1 DBIx::Exception::Deadlock
+
+Inherits from L</DBIx::Exception>.
+
+=head1 DBIx::Exception::Syntax
+
+Inherits from L</DBIx::Exception>.
+
+=head2 near
+
+The token that the syntax error was near
+
+=head1 DBIx::Exception::NoSuchTable
+
+Inherits from L</DBIx::Exception>.
+
+=head2 table
+
+The table that does not exist
+
+=head1 DBIx::Exception::NoSuchColumn
+
+Inherits from L</DBIx::Exception>.
+
+=head2 table
+
+The table that for the referenced column
+
+=head2 column
+
+The column that does not exist
